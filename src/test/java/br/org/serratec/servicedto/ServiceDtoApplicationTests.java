6a32e4cd44ff6bed9498b80f9a01e0104881375d
Mariana Hiath <maarianahiath@gmail.com>
2021-11-04 18:45:02 -0300

    first commit

---
+++ src/test/java/br/org/serratec/servicedto/ServiceDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.serratec.servicedto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
